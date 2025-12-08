73de5e5eb9faa6b512d5a4562c9d2431ea2a20d5
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-12-08 12:03:35 -0400

    refactor: formata backkend e removo o check do fmt

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-pratica-devops-leo"
-    key     = "site-projeto-1.1/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket       = "terraform-state-pratica-devops-leo"
+    key          = "site-projeto-1.1/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
